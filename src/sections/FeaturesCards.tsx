de827e2b63e1f3180abae9d676a476bb5fcce9ca
nstlgy <yvsh7.btw@gmail.com>
2025-02-17 02:19:30 +0530

    feat(FeaturesCards): Add FeaturesCards component to render info cards

---
+++ src/sections/FeaturesCards.tsx
@@ -0,0 +1,23 @@
+function FeaturesCards() {
+  return (
+    <section>
+      <div className="container">
+        <h2>Discover the future of blockchain with Blockforge</h2>
+        <div>
+          <div>
+            <h3>Revolutionary Blockchain API</h3>
+            <p>
+              Effortlessly Integrate and Manage date with our cutting-edge API
+              designed for seamless connectivity
+            </p>
+            <div className="flex">
+              <button>Learn More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default FeaturesCards;
