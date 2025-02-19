7420a260321a204b5b6d4618c28bef4f5686425a
nstlgy <yvsh7.btw@gmail.com>
2025-02-19 23:52:33 +0530

    style(LatestPosts): add flex by column and gap between each card

--- src/sections/LatestPosts.tsx
+++ src/sections/LatestPosts.tsx
@@ -11,7 +11,7 @@ function LatestPosts() {
           Keep up with the newest trends, updates, and insights in the
           blockchain world, updated, weekly.
         </p>
-        <div className="mt-16">
+        <div className="mt-16 flex flex-col gap-8">
           {[...new Array(4)].fill(0).map((item, itemIndex) => (
             <Card key={itemIndex} buttonText="Read More">
               <div className="px-3 py-1.5 uppercase font-heading font-extrabold tracking-wider text-xs bg-fuchsia-500/15 text-fuchsia-500 inline-flex rounded-full">
