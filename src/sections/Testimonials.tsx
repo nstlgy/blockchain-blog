c433972d2acc08a98bc924548e598679ac5a6d08
nstlgy <yvsh7.btw@gmail.com>
2025-02-22 00:51:53 +0530

    style(Testimonials): Changed the styles

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,28 +1,36 @@
 function TestimonialsSection() {
   return (
-    <section className="py-32">
-      <div>
-        {[...new Array(3)].fill(0).map((_, index) => (
-          <div>
-            <blockquote>
-              <p>
-                The user experience is phenomenal, and the support team is
-                always there to help. Highly recommended!
-              </p>
-              <cite>
-                <div>
+    <section className="py-32 bg-zinc-800">
+      <div className="container">
+        <div>
+          {[...new Array(3)].fill(0).map((_, index) => (
+            <div>
+              <blockquote>
+                <p className="font-heading text-3xl font-black">
+                  &ldquo;The user experience is phenomenal, and the support team
+                  is always there to help. Highly recommended!&rdquo;
+                </p>
+                <cite className="mt-8 block">
                   <div>
-                    <div>avatar</div>
+                    <div>
+                      <div className="size-16 bg-zinc-700 rounded-full">
+                        avatar
+                      </div>
+                    </div>
+                    <div>
+                      <div className="text-lg not-italic font-black">
+                        Erica Wyatt
+                      </div>
+                      <div className="text-zinc-400 not-italic">
+                        Product Manager - BlockLink
+                      </div>
+                    </div>
                   </div>
-                  <div>
-                    <div>Erica Wyatt</div>
-                    <div>Product Manager - BlockLink</div>
-                  </div>
-                </div>
-              </cite>
-            </blockquote>
-          </div>
-        ))}
+                </cite>
+              </blockquote>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
