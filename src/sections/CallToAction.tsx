fbffd180f4233f0910b1218187307d118a33f83b
nstlgy <yvsh7.btw@gmail.com>
2025-02-22 14:16:21 +0530

    feat(Footer): Add Footer component

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,5 +1,6 @@
 import Hexagon from "../components/Hexagon";
 import CutCornerButton from "../components/CutCornerButton";
+import Circle from "../components/Circle";
 
 function CallToAction() {
   return (
@@ -11,6 +12,22 @@ function CallToAction() {
           </div>
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             <Hexagon className="size-[1100px]" />
+          </div>
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Circle className="absolute left-0 -top-[400px]">
+              <img
+                src="/assets/images/cuboid.png"
+                alt=""
+                className="size-[140px]"
+              />
+            </Circle>
+            <Circle className="absolute -left-[600px] -top-[70px]">
+              <img
+                src="/assets/images/cylinder.png"
+                alt=""
+                className="size-[140px]"
+              />
+            </Circle>
           </div>
           <h2 className="font-heading font-black text-4xl md:text-5xl lg:text-6xl text-center">
             Ready to <span className="block">get started?</span>
